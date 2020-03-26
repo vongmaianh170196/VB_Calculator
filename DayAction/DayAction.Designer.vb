ae4862e373dd90fc40a2b174589b584c03ca8e5a
Anh <vongmaianh170196@gmail.com>
2020-03-26 15:49:30 +0200

    Day_Action_v1

---
+++ DayAction/DayAction.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DayAction
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.timePicker = New System.Windows.Forms.DateTimePicker()
+        Me.txtAction = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'timePicker
+        '
+        Me.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time
+        Me.timePicker.Location = New System.Drawing.Point(22, 23)
+        Me.timePicker.Name = "timePicker"
+        Me.timePicker.Size = New System.Drawing.Size(168, 22)
+        Me.timePicker.TabIndex = 0
+        '
+        'txtAction
+        '
+        Me.txtAction.AutoSize = True
+        Me.txtAction.Location = New System.Drawing.Point(19, 58)
+        Me.txtAction.Name = "txtAction"
+        Me.txtAction.Size = New System.Drawing.Size(174, 17)
+        Me.txtAction.TabIndex = 2
+        Me.txtAction.Text = "State has not been set yet"
+        '
+        'DayAction
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(354, 302)
+        Me.Controls.Add(Me.txtAction)
+        Me.Controls.Add(Me.timePicker)
+        Me.Name = "DayAction"
+        Me.Text = "Day Action"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents timePicker As DateTimePicker
+    Friend WithEvents txtAction As Label
+End Class
---
+++ DayAction/DayAction.vb
@@ -0,0 +1,41 @@
+﻿Public Class DayAction
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles timePicker.ValueChanged
+        Me.Hour = timePicker.Value.Hour
+    End Sub
+    Private Enum Action As Integer
+        Sleeping = 1
+        Awake = 2
+        Working = 3
+        Eating = 4
+        Resting = 5
+        Gymming = 6
+    End Enum
+    'Private state As Action
+    Private Property Hour() As Integer
+        Get
+            Return timePicker.Value.Hour
+        End Get
+        Set(value As Integer)
+            timePicker.Value = New Date(Now.Year, Now.Month, Now.Day, value, 0, 0)
+            Dim state As Action
+            If value > 6 And value <= 8 Then
+                state = Action.Awake
+            ElseIf value > 8 And value < 13 Then
+                state = Action.Eating
+            ElseIf value >= 13 And value < 18 Then
+                state = Action.Working
+            ElseIf value >= 18 And value < 19 Then
+                state = Action.Resting
+            ElseIf value >= 19 And value < 22 Then
+                state = Action.Gymming
+            Else state = Action.Sleeping
+            End If
+            txtAction.Text = "At " & value & " :00, I am " & state.ToString()
+
+        End Set
+    End Property
+
+    Private Sub DayAction_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Hour = Now.Hour
+    End Sub
+End Class
